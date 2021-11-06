24f563df765adc638849af5251e75481e76c4881
Sana Alshahrani <me-sa51@hotmail.com>
2021-11-06 23:40:06 +0300

    Sana Alshahrani

---
+++ AttendanceApp/AttendanceApp/AttendanceModel.swift
@@ -0,0 +1,24 @@
+//
+//  AttendanceModel.swift
+//  AttendanceApp
+//
+//  Created by Sana Alshahrani on 30/03/1443 AH.
+//
+
+import Foundation
+
+struct AttendanceListModel: Codable {
+    let name: String
+    var isPresent: Bool
+}
+
+struct AttendanceFinal: Codable {
+    let date: String
+    let teacherID: String
+    var presentNumber: Int
+    var absentNumber: Int
+    let attendance: [AttendanceListModel]
+}
+struct Students {
+    let name: String
+}
