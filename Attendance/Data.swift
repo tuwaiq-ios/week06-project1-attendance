dc02c66bdf7b5d68b6ebf4979e0eb0801887a44c
tasnim <tasneemjafsher@gmail.com>
2021-11-06 21:48:23 +0300

    m

---
+++ Attendance/Data.swift
@@ -0,0 +1,22 @@
+//
+//  Data.swift
+//  Attendance
+//
+//  Created by Tsnim Alqahtani on 28/03/1443 AH.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Student {
+    let name: String
+    var attendance: Bool
+}
+
+ struct Days {
+    let name : String
+     var Date1 : Timestamp
+    let id : Int
+   
+//     let Date1 = UIDatePicker()
+}
