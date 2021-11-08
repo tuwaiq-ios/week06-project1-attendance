9149c94c63e72d0f759de5ee2318e070bd622128
HANAN <hanan@HANANMacBook-Pro.local>
2021-11-08 05:55:53 +0300

    Hanan

---
+++ AttendanceApp/AttendanceApp/AttendanceModel.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AttendanceApp
+//
+//  Created by HANAN on 28/03/1443 AH.
+//
+
+import Foundation
+
+struct AttendanceListModel: Codable {
+    let name: String
+    var isPresent: Bool
+}
+
+struct AttendanceFinal: Codable {
+    let date: String
+    let teacherID: String
+    var presentNumber: Int
+    var absentNumber: Int
+    let attendance: [AttendanceListModel]
+}
+struct Students {
+    let name: String
+}
